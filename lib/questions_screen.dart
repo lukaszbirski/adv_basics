9450a8cbfa02e9d6e9f543e9ce710c2fd4d63bd9
Lukasz Birski <lukasz.birski@gmail.com>
2025-02-03 23:19:00 +0100

    Finished button styling

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:adv_basics/models/quiz_question.dart';
+import 'package:flutter/material.dart';
+
+import 'answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,8 +14,40 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreen extends State<QuestionsScreen> {
+
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text("QUESTION SCREEN");
+
+    final QuizQuestion question = questions[questionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              question.text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 30),
+            ...question.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: answerQuestion,);
+            })
+          ],
+        ),
+      )
+    );
   }
 }
